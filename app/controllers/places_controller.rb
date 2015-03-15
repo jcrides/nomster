0a3c7980115df364c0731df20d2ed870d899c686
Jeremy Carey <jeremycar@gmail.com>
2015-03-14 22:16:32 -0700

    sort places descending by creation date/time on index view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
 
   def index
-    @places = Place.paginate(:page => params[:page])
+    @places = Place.paginate(:page => params[:page]).order('created_at DESC')
   end
 
   def new
