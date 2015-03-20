8b9bf0a8543ca99021c74e798a88f98f1ebd3554
Jeremy Carey <jeremycar@gmail.com>
2015-03-19 21:20:56 -0700

    add user's places to their profile page (lsn 51)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @places = Place.where("user_id = #{params[:id]}").paginate(:page => params[:page]).order('created_at DESC')
   end
 
 end
