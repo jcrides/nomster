076b58fd5d203cf929b74e38e745c35e7eeba84a
Jeremy Carey <jeremycar@gmail.com>
2015-03-22 23:23:23 -0700

    add includes for :comments and :photos to place in user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @places = Place.where("user_id = #{params[:id]}").paginate(:page => params[:page]).order('created_at DESC')
+    @places = Place.includes(:comments, :photos).where("user_id = #{params[:id]}").paginate(:page => params[:page]).order('created_at DESC')
   end
 
 end
